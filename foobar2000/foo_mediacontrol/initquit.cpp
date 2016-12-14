e2b1b7cda0881ec27243e46876f52a5d3945d492
Nikola Yanakiev <nikola.yanakiev0@gmail.com>
2016-12-14 18:52:41 +0200

    Pre-release cleanup.

--- foobar2000/foo_mediacontrol/initquit.cpp
+++ foobar2000/foo_mediacontrol/initquit.cpp
@@ -7,15 +7,11 @@
 class myinitquit : public initquit {
 public:
 	void on_init() {
-		console::print("on_init()");
-
 		// create a playback listener
 		m_playback_listener = new playback_listener();
 		m_message_listener = new message_listener();
 	}
 	void on_quit() {
-		console::print("on_quit()");
-		
 		// free memory of playback listener
 		delete m_playback_listener;
 		delete m_message_listener;
